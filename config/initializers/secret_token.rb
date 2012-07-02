7030cda113529a59cc9446ae14d0a4b03135898e
Mark Gangl & Trace Wax <pair+mark+trace@pivotallabs.com>
2012-07-02 11:59:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectmonitorCiTest::Application.config.secret_token = 'a25c47dc21d8362c00edcb45390eec9a2941c6e6628f1e158087171a7e5fa84af4c6f98f3b49436902103fc68cdc69222477b084de89cb324ab6e5e7b6256610'
