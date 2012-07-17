2defba7a4502e4b632a62a5f7b96a170800af66a
Andrew Fader & Mark Gangl <pair+afader+mgangl@pivotallabs.com>
2012-07-17 16:56:36 -0400

    only sleep for 2 min

--- spec/sleepy_spec.rb
+++ spec/sleepy_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe "sleeping" do
-  it "sleeps for 45 minutes so that we can see specs running" do
+  it "sleeps for X minutes so that we can see specs running" do
     expect {
-      sleep( 45 * 60 )
+      sleep( 2 * 60 )
     }.to change{ Time.now }
   end
 end
